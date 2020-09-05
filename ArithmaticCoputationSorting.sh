b53989bf585462c7b89f75f483183042227023fe
RutujaBiradar <36528715+RutujaBiradar@users.noreply.github.com>
2020-09-05 03:19:34 -0400

    Added : Arithmatic Computation Array

---
+++ ArithmaticCoputationSorting.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+echo "Arithmetic Compution And Sorting"
+
+declare -A arithmaticOperation
+
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
+
+result=$(($firstInput + $secondInput * $thirdInput))
+echo "Result:" $result
+
+resultOne=$(($firstInput * $secondInput + $thirdInput))
+echo "Result:" $resultOne
+
+resultTwo=$(($thirdInput + $firstInput / $secondInput))
+echo "Result:" $resultTwo
+
+resultThree=$(($firstInput % $secondInput + $thirdInput))
+echo "Result:" $resultThree
+resultThree=$(($firstInput % $secondInput + $thirdInput))
+echo "Result:" $resultThree
+
+
+
+arithmaticOperation[result]=$result
+arithmaticOperation[resultOne]=$resultOne
+arithmaticOperation[resultTwo]=$resultTwo
+arithmaticOperation[resultThree]=$resultThree
+
+
+for((index=0; index<=${#arithmaticOperation[@]}; index++))
+do
+  array[index]=${#arithmaticOperation[result$(($index+1))]}
+done
+echo "${array[@]}"
