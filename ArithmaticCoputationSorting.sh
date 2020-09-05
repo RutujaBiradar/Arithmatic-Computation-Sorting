6d206353e3e7ee22bfca10bb6e3de6120b96c8f3
RutujaBiradar <36528715+RutujaBiradar@users.noreply.github.com>
2020-09-05 02:52:12 -0400

    Added : Arithmatic Computation 4

---
+++ ArithmaticCoputationSorting.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+echo "Arithmetic Compution And Sorting"
+
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
+
+result=$(($firstInput + $secondInput * $thirdInput))
+echo "Result:" $result
+
+resultOne=$(($firstInput * $secondInput + $thirdInput))
+echo "Result:" $resultOne
+
+resultTwo=$(($thirdInput + $firstInput / $secondInput))
+echo "Result:" $resultTwo
+
+resultThree=$(($firstInput % $secondInput + $thirdInput))
+echo "Result:" $resultThree
